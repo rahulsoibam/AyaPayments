6b32df01dae91ec456737778774ea512fdac688b
Rahul Soibam <rahulsoibam@gmail.com>
2021-10-28 18:31:13 +0530

    Tests added

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"strings"
 )
 
 type Square struct {
-	posX              int
-	posY              int
+	posX              byte
+	posY              byte
 	algebraicNotation string
 	path              string
 }
 
-func InitSquare(x int, y int) (*Square, error) {
+// Factory pattern used because in Go, struct does not have constructors
+// Create a Square using the numerical representation of the knight's position
+func InitSquare(x byte, y byte) (*Square, error) {
 	// Check if Square is inside board
 	if (x >= 1 && x <= 8) && (y >= 1 && y <= 8) {
 		return &Square{
@@ -27,23 +30,32 @@ func InitSquare(x int, y int) (*Square, error) {
 	return nil, errors.New("Square is not inside board")
 }
 
+// Create a Square using the algebraic notation of the knight's position
 func InitSquareFromAlgebraicNotation(algNotation string) (*Square, error) {
+	// Convert to uppercase
+	algNotation = strings.ToUpper(algNotation)
+
+	// if the algebraic notation contains more than 2 components, return error
 	if len(algNotation) != 2 {
-		return nil, errors.New("Invalid algebraic notation")
+		return nil, errors.New("Algebraic expression too long")
 	} else {
-		x, y := int(algNotation[0]-'A'+1), int(algNotation[1]-'0')
+		// Simple and readable way to convert the byte into a number.
+		x, y := algNotation[0]-'A'+1, algNotation[1]-'0'
 		// fmt.Println("x: ", x, " y: ", y)
 		square, err := InitSquare(x, y)
 		return square, err
 	}
 }
 
-func (source *Square) ShortestPathToaSquare(target *Square) string {
+func (source *Square) ShortestPathToASquare(target *Square) (string, error) {
 	// Possible knight moves in X and Y direction
-	dx := []int{-2, -1, 1, 2, -2, -1, 1, 2}
-	dy := []int{-1, -2, -2, -1, 1, 2, 2, 1}
+	// DO NOT CHANGE THIS AS THERE ARE MULTIPLE SOLUTIONS
+	// AND CHANGING THIS WILL GIVE DIFFERENT SOLUTIONS.
+	// IF CHANGE IS NECESSARY, UPDATE THE TESTS ACCORDINGLY
+	dx := []int{-1, 2, 1, -2, -2, -1, 1, 2}
+	dy := []int{-2, -1, -2, -1, 1, 2, 2, 1}
 
-	// Store visited squares
+	// Store visited squares. Default values should be false as none of the squares are visited.
 	visited := [][]bool{
 		{false, false, false, false, false, false, false, false},
 		{false, false, false, false, false, false, false, false},
@@ -56,7 +68,11 @@ func (source *Square) ShortestPathToaSquare(target *Square) string {
 	}
 
 	// Queue for Breadth First Search
-	// a slice can be used but will be prone to memory leaks
+	// a slice can be used like so
+	// queue := []Square{}
+	// to dequeue: current, queue := queue[0], queue[1:]
+	// could be used. But this slice approach will cause
+	// MEMORY LEAKS
 	queue := list.New()
 
 	// mark the source square as visited
@@ -74,7 +90,7 @@ func (source *Square) ShortestPathToaSquare(target *Square) string {
 
 		// Check if the current square is the target
 		if current.posX == target.posX && current.posY == target.posY {
-			return current.path
+			return current.path, nil
 		}
 
 		for i := 0; i < 8; i++ {
@@ -83,7 +99,9 @@ func (source *Square) ShortestPathToaSquare(target *Square) string {
 
 			if (x >= 1 && x <= 8) && (y >= 1 && y <= 8) && !visited[x-1][y-1] {
 				visited[x-1][y-1] = true
-				visiting, err := InitSquare(x, y)
+				// byte(x) and byte(y) is the simplest here it is safe
+				// because both x and y cannot be less than 1 or more than 8
+				visiting, err := InitSquare(byte(x), byte(y))
 				if err != nil {
 					log.Println(err)
 					continue
@@ -97,7 +115,7 @@ func (source *Square) ShortestPathToaSquare(target *Square) string {
 			}
 		}
 	}
-	return ""
+	return "", errors.New("Given the right inputs, a solution is always possible. But no solution is found.")
 
 }
 
@@ -108,17 +126,34 @@ func main() {
 	// }
 	// fmt.Println(square)
 
-	source, err := InitSquareFromAlgebraicNotation("A8")
+	var sourceInputString string
+	var targetInputString string
+	fmt.Println("Enter the source and target squares in algebraic notation")
+	_, err := fmt.Scan(&sourceInputString)
+	if err != nil {
+		log.Println(err)
+	}
+	_, err = fmt.Scan(&targetInputString)
+	if err != nil {
+		log.Println(err)
+	}
+
+	source, err := InitSquareFromAlgebraicNotation(sourceInputString)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	target, err := InitSquareFromAlgebraicNotation("B7")
+	target, err := InitSquareFromAlgebraicNotation(targetInputString)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	fmt.Println(source.ShortestPathToaSquare(target))
+	path, err := source.ShortestPathToASquare(target)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(path)
 
 }
---
+++ main_test.go
@@ -0,0 +1,91 @@
+package main
+
+import "testing"
+
+func TestInitSquare(t *testing.T) {
+	var x byte
+	var y byte
+	x = 1
+	y = 2
+	square, err := InitSquare(x, y)
+	if err != nil {
+		t.Errorf("%v", err)
+	} else if square.posX != x || square.posY != y || square.algebraicNotation != "A2" || square.path != "" {
+		t.Error("Expected not returned")
+
+	}
+}
+func TestInitSquareFromAlgebraicNotation(t *testing.T) {
+	var algNotation = "A5"
+	square, err := InitSquareFromAlgebraicNotation(algNotation)
+	if err != nil {
+		t.Errorf("%v", err)
+	} else if square.posX != 1 || square.posY != 5 || square.algebraicNotation != "A5" || square.path != "" {
+		t.Error("Expected not returned")
+	}
+}
+
+func TestShortestPathToASquare(t *testing.T) {
+	source, err := InitSquareFromAlgebraicNotation("A8")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	target, err := InitSquareFromAlgebraicNotation("B7")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	path, err := source.ShortestPathToASquare(target)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Expected output will change based on the dx and dy arrays in main.go. Update accordingly.
+	expected := "C7 B5 D6 B7"
+	if path != expected {
+		t.Errorf("Incorrect path. Expected %q got %q", expected, path)
+		t.Error("If the solution is valid, change the dx and dy arrays in main.go or update test")
+	}
+
+	source2, err := InitSquareFromAlgebraicNotation("A1")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	target2, err := InitSquareFromAlgebraicNotation("H8")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	path2, err := source2.ShortestPathToASquare(target2)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	// Expected output will change based on the dx and dy arrays in main.go. Update accordingly.
+	// Too many possible paths, not practical to list them all.
+	expected2 := "B3 D2 C4 D6 F7 H8"
+	if path2 != expected2 {
+		t.Errorf("Incorrect path. Expected %q got %q", expected2, path2)
+		t.Error("If the solution is valid, change the direction matrix in main.go or update test")
+	}
+
+	source3, err := InitSquareFromAlgebraicNotation("A8")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	target3, err := InitSquareFromAlgebraicNotation("A8")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	path3, err := source3.ShortestPathToASquare(target3)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	// Expected output will change based on the dx and dy arrays in main.go. Update accordingly.
+	// Too many possible paths, not practical to list them all.
+	expected3 := ""
+	if path3 != expected3 {
+		t.Errorf("Incorrect path. Expected %q got %q", expected3, path3)
+		t.Error("If the solution is valid, change the direction matrix in main.go or update test")
+	}
+}
